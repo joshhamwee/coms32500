922a197753eb57cda282053fc8310a37cae1b922
joshhamwee <joshhamwee@me.com>
2020-03-20 12:52:34 +0000

    Made table dynamic and styled it!

--- public/scripts/create_table.js
+++ public/scripts/create_table.js
@@ -11,55 +11,83 @@ function receive() {
   if (this.readyState != XMLHttpRequest.DONE) return;
   var list = JSON.parse(this.responseText);
   console.log(list);
-  //var html = "<li>" + list.  ;
-  //var ul = document.querySelector("#banklist");
-  //ul.innerHTML = html;
-  $(function() {
-    //create Tabulator on DOM element with id "banktable"
-    $("#banktable").tabulator({
-      //height: "320px",
-      fitColumns: true,
-      columns: [
-        { title: "Name", field: "name", sorter: "string", width: 150 },
-        {
-          title: "Location",
-          field: "location",
-          sorter: "string",
-          align: "center"
-        },
-        {
-          title: "Category of Bank",
-          field: "category",
-          sorter: "string"
-        },
-        {
-          title: "Number of Empolyees",
-          field: "size_employee",
-          sorter: "number",
-          formatter: "progress"
-        },
-        {
-          title: "Year 1 Salary",
-          field: "y1_salary",
-          formatter: "money",
-          sorter: "number"
-        },
 
-        {
-          title: "Job Competitiveness",
-          field: "competitive",
-          formatter: "star",
-          sorter: "number"
+  if (window.innerWidth < 750) {
+    $(function() {
+      //create Tabulator on DOM element with id "banktable"
+      $("#banktable").tabulator({
+        //height: "320px",
+        fitColumns: true,
+        columns: [
+          { title: "Name", field: "name", sorter: "string" },
+          {
+            title: "Category of Bank",
+            field: "category",
+            sorter: "string"
+          },
+          {
+            title: "Number of Empolyees",
+            field: "size_employee",
+            sorter: "number",
+            formatter: "plaintext"
+          }
+        ],
+        rowClick: function(e, id, data, row) {
+          var url = "bank.html?id=" + id;
+          window.location.href = url;
         }
-      ],
-      rowClick: function(e, id, data, row) {
-        var url = "bank.html?id=" + id;
-        window.location.href = url;
-      }
+      });
+      //define some sample data
+      var tabledata = list;
+      //load sample data into the table
+      $("#banktable").tabulator("setData", tabledata);
     });
-    //define some sample data
-    var tabledata = list;
-    //load sample data into the table
-    $("#banktable").tabulator("setData", tabledata);
-  });
+  } else {
+    $(function() {
+      //create Tabulator on DOM element with id "banktable"
+      $("#banktable").tabulator({
+        //height: "320px",
+        fitColumns: true,
+        columns: [
+          { title: "Name", field: "name", sorter: "string" },
+          {
+            title: "Category of Bank",
+            field: "category",
+            sorter: "string"
+          },
+          {
+            title: "Number of Empolyees",
+            field: "size_employee",
+            sorter: "number",
+            formatter: "plaintext"
+          },
+          {
+            title: "Year 1 Salary",
+            field: "y1_salary",
+            formatter: "money",
+            sorter: "number"
+          },
+
+          {
+            title: "Job Competitiveness",
+            field: "competitive",
+            formatter: "star",
+            sorter: "number"
+          }
+        ],
+        rowClick: function(e, id, data, row) {
+          var url = "bank.html?id=" + id;
+          window.location.href = url;
+        }
+      });
+      //define some sample data
+      var tabledata = list;
+      //load sample data into the table
+      $("#banktable").tabulator("setData", tabledata);
+    });
+  }
 }
+
+window.onresize = function() {
+  location.reload();
+};
