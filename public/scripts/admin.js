9f49746b4c8b95d963be79a451bfab30fd27fe2d
joshhamwee <joshhamwee@me.com>
2020-04-18 14:51:42 +0100

    Fixed JS issue

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,15 +1,24 @@
-$(".tabs").each(function(index) {
-  var $tabParent = $(this);
-  var $tabs = $tabParent.find("li");
-  var $contents = $tabParent.next(".tabs-content").find(".tab-content");
+document.querySelectorAll(".tabs").forEach(function (elem) {
+  tabParent = document.querySelector(".tabs");
+  tabs = elem.querySelectorAll("li");
+  contents = elem.nextElementSibling;
+  contentsList = contents.querySelectorAll(".tab-content");
 
-  $tabs.click(function() {
-    var curIndex = $(this).index();
-    // toggle tabs
-    $tabs.removeClass("is-active");
-    $tabs.eq(curIndex).addClass("is-active");
-    // toggle contents
-    $contents.removeClass("is-active");
-    $contents.eq(curIndex).addClass("is-active");
+  tabs.forEach(function (elem2) {
+    elem2.addEventListener("click", function () {
+      curIndex = getNodeindex(elem2);
+
+      for (var i = 0; i < 3; i++) {
+        tabs[i].classList.remove("is-active");
+        contentsList[i].classList.remove("is-active");
+      }
+
+      contentsList[curIndex].classList.add("is-active");
+      elem2.classList.add("is-active");
+    });
   });
 });
+
+function getNodeindex(elm) {
+  return [...elm.parentNode.children].indexOf(elm);
+}
